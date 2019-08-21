37b858c5cbeea0588f211ee4c3d416e085af4cf7
saaz14 <aqeel.zaidi@gmail.com>
2019-08-21 18:38:35 -0400

    Updated part 2 of homework18thaug

--- src/HomeworkAug18th/Part2.java
+++ src/HomeworkAug18th/Part2.java
@@ -1,17 +1,108 @@
 package HomeworkAug18th;
 
 public class Part2 {
-
 	public static void main(String[] args) {
+
+		String str1 = "computer";
+		String str2 = "Java";
+		String str3 = "Pizza";
+
+		if (str1.length() > 5) {
+			System.out.println("1st String name <" + str1 + "> has a total of " + str1.length()
+			+ " characters and the last 5 characters are \'" + str1.substring(str1.length() - 5) + "\'");
 		
-		String str1 [] = {"computer"}; 
-		String str2 [] = {"Java"}; 
-		String str3 []= {"Pizza"};
-		
-		
-		}
+		} 
+		if (str1.length() == 5) {
+			System.out.println("1st String name <" + str1 + "> has a total of " + str1.length() + " Characters");
 			
-
+		}
+		
+		
+		if (str1.length() == 4) {
+			System.out.println("1st String name <" + str1 + "> has a total of " + str1.length()
+			+ " characters and the last 5 characters are \'" + str1.concat("A") + "\'");
+		}
+		if (str1.length() == 3) {
+			System.out.println("1st String name <" + str1 + "> has a total of " + str1.length()
+			+ " characters and the last 5 characters are \'" + str1.concat("AA") + "\'");
+		}
+		
+		if (str1.length() == 2) {
+			System.out.println("1st String name <" + str1 + "> has a total of " + str1.length()
+			+ " characters and the last 5 characters are \'" + str1.concat("AAA") + "\'");
+		}
+		
+		if (str1.length() == 1) {
+			System.out.println("1st String name <" + str1 + "> has a total of " + str1.length()
+			+ " characters and the last 5 characters are \'" + str1.concat("AAAA") + "\'");
+		}
+		
+		//----------------------------------------------------------------------------------------
+		
+		
+		if (str2.length() > 5) {
+			System.out.println("2nd String name <" + str2 + "> has a total of " + str2.length()
+			+ " characters and the last 5 characters are \'" + str2.substring(str2.length() - 5) + "\'");
+		
+		} 
+		if (str2.length() == 5) {
+			System.out.println("2nd String name <" + str2 + "> has a total of " + str2.length() + " Characters");
+			
+		}
+		
+		
+		if (str2.length() == 4) {
+			System.out.println("2nd String name <" + str2 + "> has a total of " + str2.length()
+			+ " characters and the last 5 characters are \'" + str2.concat("A") + "\'");
+		}
+		if (str2.length() == 3) {
+			System.out.println("2nd String name <" + str2 + "> has a total of " + str2.length()
+			+ " characters and the last 5 characters are \'" + str2.concat("AA") + "\'");
+		}
+		
+		if (str2.length() == 2) {
+			System.out.println("2nd String name <" + str2 + "> has a total of " + str2.length()
+			+ " characters and the last 5 characters are \'" + str2.concat("AAA") + "\'");
+		}
+		
+		if (str2.length() == 1) {
+			System.out.println("2nd String name <" + str2 + "> has a total of " + str2.length()
+			+ " characters and the last 5 characters are \'" + str2.concat("AAAA") + "\'");
+		}
+		
+		
+		//---------------------------------------------------------------------------------------
+		
+		if (str3.length() > 5) {
+			System.out.println("3rd String name <" + str3 + "> has a total of " + str3.length()
+			+ " characters and the last 5 characters are \'" + str3.substring(str3.length() - 5) + "\'");
+		
+		} 
+		if (str3.length() == 5) {
+			System.out.println("3rd String name <" + str3 + "> has a total of " + str3.length() + " Characters");
+			
+		}
+		
+		
+		if (str3.length() == 4) {
+			System.out.println("3rd String name <" + str3 + "> has a total of " + str3.length()
+			+ " characters and the last 5 characters are \'" + str3.concat("A") + "\'");
+		}
+		if (str3.length() == 3) {
+			System.out.println("3rd String name <" + str3 + "> has a total of " + str3.length()
+			+ " characters and the last 5 characters are \'" + str3.concat("AA") + "\'");
+		}
+		
+		if (str3.length() == 2) {
+			System.out.println("3rd String name <" + str3 + "> has a total of " + str3.length()
+			+ " characters and the last 5 characters are \'" + str3.concat("AAA") + "\'");
+		}
+		
+		if (str3.length() == 1) {
+			System.out.println("2nd String name <" + str3 + "> has a total of " + str3.length()
+			+ " characters and the last 5 characters are \'" + str3.concat("AAAA") + "\'");
+		}
+		
+		
 	}
-
 }
